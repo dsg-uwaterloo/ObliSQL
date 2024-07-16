4d7638343c887e53d3bab43b625deb928fa95c7b
nachiketsrao <nachiketsrao@gmail.com>
2024-07-15 21:27:14 -0400

    add cryptography functions

--- pathOram/oram/oram.go
+++ pathOram/oram/oram.go
@@ -1,15 +1,15 @@
 package oram
 
 import (
-	// mathrand "math/rand"
-	// "crypto/aes"
-	// "crypto/cipher"
-	// "crypto/rand"
+	mathrand "math/rand"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	// "encoding/json"
-	// "errors"
+	"errors"
 	// "fmt"
 	// "strings"
-	// "io"
+	"io"
     // "github.com/go-redis/redis/v8" // using go-redis library
 	// "context"
 )
@@ -25,4 +25,57 @@ type Block struct {
 type Bucket struct {
 	Blocks        []Block
 	RealBlockCount int
+}
+
+// ############################### Cryptography functions #######################################
+
+func getRandomInt(max int) int {
+    return mathrand.Intn(max)
+}
+
+func generateRandomKey() ([]byte, error) {
+	key := make([]byte, 32) // AES-256
+	_, err := io.ReadFull(rand.Reader, key)
+	if err != nil {
+		return nil, err
+	}
+	return key, nil
+}
+
+func encrypt(data []byte, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(data))
+	iv := ciphertext[:aes.BlockSize]
+
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], data)
+
+	return ciphertext, nil
+}
+
+func decrypt(data []byte, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(data) < aes.BlockSize {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	iv := data[:aes.BlockSize]
+	ciphertext := data[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(ciphertext, ciphertext)
+
+	return ciphertext, nil
 }
