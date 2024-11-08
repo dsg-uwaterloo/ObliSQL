1cbf7b37df2e83acf05887db38cfb67d5d377480
nachiketsrao <nachiketsrao@gmail.com>
2024-11-07 21:18:41 -0500

    Initialize ORAM DB with tracefile

--- pkg/oramExecutor/oramExecutor.go
+++ pkg/oramExecutor/oramExecutor.go
@@ -1,19 +1,20 @@
 package oramexecutor
 
 import (
+	"bufio"
 	"context"
 	"fmt"
+	"os"
+	"strings"
 
 	executor "github.com/project/ObliSql/api/oramExecutor"
-
-	"github.com/golang/protobuf/ptypes/wrappers"
 	// "github.com/redis/go-redis/v9"
 )
 
 const (
-	logCapacity = 10   // Logarithm base 2 of capacity (1024 buckets)
-	Z           = 5    // Number of blocks per bucket
-	stashSize   = 2000 // Maximum number of blocks in stash
+	logCapacity = 15     // Logarithm base 2 of capacity (1024 buckets)
+	Z           = 5      // Number of blocks per bucket
+	stashSize   = 100000 // Maximum number of blocks in stash
 )
 
 type MyOram struct {
@@ -31,7 +32,7 @@ func (e MyOram) ExecuteBatch(ctx context.Context, req *executor.RequestBatchORAM
 	fmt.Printf("Got a request with ID: %d \n", req.RequestId)
 
 	// set batchsize
-	batchSize := 50
+	batchSize := 10
 
 	// Batching(requests []request.Request, batchSize int)
 
@@ -67,13 +68,6 @@ func (e MyOram) ExecuteBatch(ctx context.Context, req *executor.RequestBatchORAM
 		replyKeys = append(replyKeys, batchKeys...)
 		replyVals = append(replyVals, returnValues...)
 
-		// TODO: create a list of Request objects with Type and key and value
-
-		// TODO: call o.Batching(requests, batch size) this will return a list of VALUES
-
-		// TODO: handle it like replyKeys = append(replyKeys, rec_block.Key)
-		// replyVals = append(replyVals, rec_block.Value)
-
 	}
 
 	return &executor.RespondBatchORAM{
@@ -83,52 +77,46 @@ func (e MyOram) ExecuteBatch(ctx context.Context, req *executor.RequestBatchORAM
 	}, nil
 }
 
-func (e MyOram) InitDb(ctx context.Context, req *executor.RequestBatchORAM) (*wrappers.BoolValue, error) {
-	fmt.Printf("Initialize DB with Key Size: %d \n", len(req.Keys))
+// func (e MyOram) InitDb(ctx context.Context, req *executor.RequestBatchORAM) (*wrappers.BoolValue, error) {
+// 	fmt.Printf("Initialize DB with Key Size: %d \n", len(req.Keys))
 
-	e.o.RedisClient.FlushData()
-	e.o.ClearKeymap()
-	e.o.ClearStash()
-	// var pairs []interface{}
+// 	e.o.RedisClient.FlushData()
+// 	e.o.ClearKeymap()
+// 	e.o.ClearStash()
 
-	// TODO: split keys and values by batchsize
-	// TODO: call o.Batching(requests, batch size) this will return a list of VALUES - can be neglected
+// 	// set batchsize
+// 	batchSize := 50
 
-	// TODO: need to tune oram.go to distinguish between put and get requests based on presence of value in requests object
+// 	for start := 0; start < len(req.Values); start += batchSize {
+// 		var requestList []Request
 
-	// set batchsize
-	batchSize := 50
+// 		end := start + batchSize
+// 		if end > len(req.Values) {
+// 			end = len(req.Values) // Ensure we don't go out of bounds
+// 		}
 
-	for start := 0; start < len(req.Values); start += batchSize {
-		var requestList []Request
+// 		// Slice the keys and values for the current batch
+// 		batchKeys := req.Keys[start:end]
+// 		batchValues := req.Values[start:end]
 
-		end := start + batchSize
-		if end > len(req.Values) {
-			end = len(req.Values) // Ensure we don't go out of bounds
-		}
+// 		for i := range batchKeys {
+// 			//pairs = append(pairs, req.Keys[i], req.Values[i])
+// 			// Read operation
+// 			currentRequest := Request{
+// 				Key:   batchKeys[i],
+// 				Value: batchValues[i],
+// 			}
 
-		// Slice the keys and values for the current batch
-		batchKeys := req.Keys[start:end]
-		batchValues := req.Values[start:end]
+// 			requestList = append(requestList, currentRequest)
+// 		}
+// 		e.o.Batching(requestList, batchSize)
+// 	}
+// 	fmt.Println("Finished Initializing DB!")
+// 	return &wrappers.BoolValue{Value: true}, nil
 
-		for i := range batchKeys {
-			//pairs = append(pairs, req.Keys[i], req.Values[i])
-			// Read operation
-			currentRequest := Request{
-				Key:   batchKeys[i],
-				Value: batchValues[i],
-			}
+// }
 
-			requestList = append(requestList, currentRequest)
-		}
-		e.o.Batching(requestList, batchSize)
-	}
-	fmt.Println("Finished Initializing DB!")
-	return &wrappers.BoolValue{Value: true}, nil
-
-}
-
-func NewORAM(LogCapacity, Z, StashSize int, redisAddr string) (*MyOram, error) {
+func NewORAM(LogCapacity, Z, StashSize int, redisAddr string, tracefile string) (*MyOram, error) { // pass in tracefile
 	key, err := GenerateRandomKey()
 	if err != nil {
 		return nil, err
@@ -154,6 +142,47 @@ func NewORAM(LogCapacity, Z, StashSize int, redisAddr string) (*MyOram, error) {
 	}
 
 	oram.initialize()
+
+	// Load data from tracefile and create Request objects
+	var requests []Request
+	file, err := os.Open(tracefile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open tracefile: %v", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		// Only process lines that start with "SET"
+		if strings.HasPrefix(line, "SET") {
+			parts := strings.SplitN(line, " ", 3)
+			if len(parts) != 3 {
+				continue // skip lines that don't have exactly 3 parts
+			}
+			key := parts[1]
+			value := parts[2]
+			requests = append(requests, Request{Key: key, Value: value})
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("error reading tracefile: %v", err)
+	}
+
+	// Initialize DB with tracefile contents
+	batchSize := 10
+
+	for start := 0; start < len(requests); start += batchSize {
+
+		end := start + batchSize
+		if end > len(requests) {
+			end = len(requests) // Ensure we don't go out of bounds
+		}
+
+		oram.Batching(requests[start:end], batchSize)
+	}
+	fmt.Println("Finished Initializing DB!")
 
 	myOram := &MyOram{
 		o: oram,
