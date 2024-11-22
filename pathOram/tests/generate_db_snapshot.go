ef887d56c594c766a65d21173ce74902d45ee41c
nachiketsrao <nachiketsrao@gmail.com>
2024-11-22 00:50:09 -0500

    New files to enable generating and testing snapshot

---
+++ pathOram/tests/generate_db_snapshot.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"pathOram/pkg/oram/oram"
+	"pathOram/pkg/oram/request"
+	"pathOram/pkg/oram/utils"
+
+	"github.com/schollz/progressbar/v3"
+)
+
+/*
+The problem is - we need to pass in the same key to redis client for both generated and
+testing redis client. otherwise it tries to decrypt with a different key -> doesn't find
+the values it's looking for cuz the decryption results in an arbitrary value/object.
+The batching mechanism returns a -1 for any key it didn't find.
+*/
+
+const (
+	logCapacity = 10 // Logarithm base 2 of capacity (1024 buckets)
+	Z           = 5  // Number of blocks per bucket
+	stashSize   = 20 // Maximum number of blocks in stash
+)
+
+// This function simulates operations on ORAM and stores snapshots of internal data.
+func main() {
+	key_input := "ura"
+	// Generate the SHA-256 hash of the input string
+	hash := sha256.New()
+	hash.Write([]byte(key_input))
+
+	// Return the 256-bit (32-byte) hash as a byte slice
+	key := hash.Sum(nil)
+
+	// -----------
+
+	// Initialize ORAM
+	o, err := oram.NewORAM(logCapacity, Z, stashSize, "127.0.0.1:6379", false, key)
+	if err != nil {
+		log.Fatalf("Error initializing ORAM: %v", err)
+	}
+	defer o.RedisClient.Close()
+
+	totalOperations := 1000 // Number of operations you plan to perform
+
+	// Create PUT requests
+	putRequests := make([]request.Request, totalOperations)
+	for i := 0; i < totalOperations; i++ {
+		key := strconv.Itoa(i)
+		putRequests[i] = request.Request{
+			Key:   key,
+			Value: fmt.Sprintf("Value%d", i),
+		}
+	}
+
+	// Batch size for processing requests
+	batchSize := 50 // Adjust batch size as needed
+
+	// Process PUT requests in batches with a progress bar
+	writeProgress := progressbar.Default(int64(totalOperations), "Writing: ")
+
+	for i := 0; i < totalOperations; i += batchSize {
+		end := i + batchSize
+		if end > totalOperations {
+			end = totalOperations
+		}
+		_, err := o.Batching(putRequests[i:end], batchSize)
+		if err != nil {
+			log.Fatalf("Error during PUT batching: %v", err)
+		}
+		writeProgress.Add(end - i)
+	}
+
+	writeProgress.Finish()
+
+	// Optionally print the ORAM's internal tree (this might be useful for debugging)
+	// utils.PrintTree(o)
+
+	// Print the stash map to inspect the internal state of ORAM
+	fmt.Println("Printing the stash...")
+	utils.PrintStashMap(o)
+
+	// =============================================================
+	// Storing Keymap and StashMap in a JSON file
+	a := o.Keymap
+	b := o.StashMap
+
+	// Prepare data to write to file
+	data := map[string]interface{}{
+		"Keymap":   a,
+		"StashMap": b,
+	}
+
+	// Open or create the snapshot file
+	file, err := os.Create("proxy_snapshot.json")
+	if err != nil {
+		log.Fatalf("Error creating file: %v", err)
+	}
+	defer file.Close()
+
+	// Serialize data to JSON and write to the file
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ") // Indentation for better readability
+	if err := encoder.Encode(data); err != nil {
+		log.Fatalf("Error encoding data to JSON: %v", err)
+	}
+
+	fmt.Println("Data written to file: proxy_snapshot.json")
+
+	// =============================================================
+
+	// Trigger a save to dump.rdb
+	fmt.Println("Triggering RDB snapshot save (BGSAVE)...")
+	err = o.RedisClient.Client.BgSave(o.RedisClient.Ctx).Err() // BGSAVE command
+	if err != nil {
+		log.Fatalf("Error triggering RDB snapshot save: %v", err)
+	}
+	fmt.Println("RDB snapshot save triggered successfully.")
+
+}
